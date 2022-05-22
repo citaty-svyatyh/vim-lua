f93721f18213dcf8a0f2ad87d835a12100bdeab9
citaty-svyatyh <46151646+citaty-svyatyh@users.noreply.github.com>
2022-05-22 09:51:01 +0300

    Update plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,129 +1,129 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function()
-	-- Packer сам себя
-	use 'wbthomason/packer.nvim'
+  -- Packer сам себя
+  use 'wbthomason/packer.nvim'
 
-	-----------------------------------------------------------
-	-- ПЛАГИНЫ ВНЕШНЕГО ВИДА
-	-----------------------------------------------------------
+  -----------------------------------------------------------
+  -- ПЛАГИНЫ ВНЕШНЕГО ВИДА
+  -----------------------------------------------------------
 
-	-- Цветовая схема
-	-- Using Packer:
+  -- Цветовая схема
+  -- Using Packer:
 
-	use 'navarasu/onedark.nvim'
-	-- use 'joshdick/onedark.vim'
-	-- use 'challenger-deep-theme/vim'
-	--- Информационная строка внизу
-	use { 'nvim-lualine/lualine.nvim',
-	requires = {'kyazdani42/nvim-web-devicons', opt = true}, }
-	-- Табы вверху
-	use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons',
-	config = function()
-		require("bufferline").setup{}
-	end, }
-
-
-	-----------------------------------------------------------
-	-- НАВИГАЦИЯ
-	-----------------------------------------------------------
-	-- Файловый менеджер
-	use { 'kyazdani42/nvim-tree.lua',
-	requires = 'kyazdani42/nvim-web-devicons',
-	config = function() require'nvim-tree'.setup {} end, }
-	-- Навигация внутри файла по классам и функциям
-	use 'majutsushi/tagbar'
-	-- fzf --
-	use {'junegunn/fzf', dir = '~/.fzf', run = './install --all' }
-	use 'junegunn/fzf.vim'
-
-	-- ack --
-	use 'mileszs/ack.vim'
-
-	-- telescope --
-	-- use { 'nvim-telescope/telescope.nvim',
-	-- requires = { {'nvim-lua/plenary.nvim'} },
-	-- config = function() require'telescope'.setup {} end, }
+  use 'navarasu/onedark.nvim'
+  -- use 'joshdick/onedark.vim'
+  -- use 'challenger-deep-theme/vim'
+  --- Информационная строка внизу
+  use { 'nvim-lualine/lualine.nvim',
+  requires = {'kyazdani42/nvim-web-devicons', opt = true}, }
+  -- Табы вверху
+  use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons',
+  config = function()
+    require("bufferline").setup{}
+  end, }
 
 
-	-----------------------------------------------------------
-	-- LSP и автодополнялка
-	-----------------------------------------------------------
+  -----------------------------------------------------------
+  -- НАВИГАЦИЯ
+  -----------------------------------------------------------
+  -- Файловый менеджер
+  use { 'kyazdani42/nvim-tree.lua',
+  requires = 'kyazdani42/nvim-web-devicons',
+  config = function() require'nvim-tree'.setup {} end, }
+  -- Навигация внутри файла по классам и функциям
+  use 'majutsushi/tagbar'
+  -- fzf --
+  use {'junegunn/fzf', dir = '~/.fzf', run = './install --all' }
+  use 'junegunn/fzf.vim'
+
+  -- ack --
+  use 'mileszs/ack.vim'
+
+  -- telescope --
+  -- use { 'nvim-telescope/telescope.nvim',
+  -- requires = { {'nvim-lua/plenary.nvim'} },
+  -- config = function() require'telescope'.setup {} end, }
 
 
-	-- Highlight, edit, and navigate code using a fast incremental parsing library
-	-- use 'nvim-treesitter/nvim-treesitter'
-	-- Additional textobjects for treesitter
-	-- use 'nvim-treesitter/nvim-treesitter-textobjects'
-	-- Collection of configurations for built-in LSP client
-	use 'neovim/nvim-lspconfig'
-	use 'williamboman/nvim-lsp-installer'
-	-- Автодополнялка
-	use 'hrsh7th/nvim-cmp'
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'hrsh7th/cmp-buffer'
-	use 'saadparwaiz1/cmp_luasnip'
-	--- Автодополнлялка к файловой системе
-	use 'hrsh7th/cmp-path'
-	-- Snippets plugin
-	use 'L3MON4D3/LuaSnip'
-	-- Снипетовая автодополнялка --
-	use 'rafamadriz/friendly-snippets'
-	-- Показывает все проблемы, кот. обноружил lsp внизу в отд. окошке
-	use 'folke/trouble.nvim'
+  -----------------------------------------------------------
+  -- LSP и автодополнялка
+  -----------------------------------------------------------
 
 
-	-----------------------------------------------------------
-	-- PYTHON
-	-----------------------------------------------------------
+  -- Highlight, edit, and navigate code using a fast incremental parsing library
+  use 'nvim-treesitter/nvim-treesitter'
+  -- Additional textobjects for treesitter
+  -- use 'nvim-treesitter/nvim-treesitter-textobjects'
+  -- Collection of configurations for built-in LSP client
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/nvim-lsp-installer'
+  -- Автодополнялка
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'saadparwaiz1/cmp_luasnip'
+  --- Автодополнлялка к файловой системе
+  use 'hrsh7th/cmp-path'
+  -- Snippets plugin
+  use 'L3MON4D3/LuaSnip'
+  -- Снипетовая автодополнялка --
+  use 'rafamadriz/friendly-snippets'
+  -- Показывает все проблемы, кот. обноружил lsp внизу в отд. окошке
+  -- use 'folke/trouble.nvim'
 
-	-- use 'python-mode/python-mode'
-	--- Шапка с импортами приводим в порядок
-	use 'fisadev/vim-isort'
-	-- Поддержка темплейтом jinja2
-	-- use 'mitsuhiko/vim-jinja'
-	use 'Glench/Vim-Jinja2-Syntax'
+
+  -----------------------------------------------------------
+  -- PYTHON
+  -----------------------------------------------------------
+
+  -- use 'python-mode/python-mode'
+  --- Шапка с импортами приводим в порядок
+  use 'fisadev/vim-isort'
+  -- Поддержка темплейтом jinja2
+  -- use 'mitsuhiko/vim-jinja'
+  use 'Glench/Vim-Jinja2-Syntax'
 
 
-	-----------------------------------------------------------
-	-- HTML и CSS
-	-----------------------------------------------------------
-	-- Подсвечивает закрывающий и откры. тэг. Если, где-то что-то не закрыто, то не подсвечивает.
-	use 'idanarye/breeze.vim'
-	-- Закрывает автоматом html и xml тэги. Пишешь <h1> и он автоматом закроется </h1>
-	use 'alvan/vim-closetag'
-	-- Подсвечивает #ffffff
-	use 'ap/vim-css-color'
+  -----------------------------------------------------------
+  -- HTML и CSS
+  -----------------------------------------------------------
+  -- Подсвечивает закрывающий и откры. тэг. Если, где-то что-то не закрыто, то не подсвечивает.
+  use 'idanarye/breeze.vim'
+  -- Закрывает автоматом html и xml тэги. Пишешь <h1> и он автоматом закроется </h1>
+  use 'alvan/vim-closetag'
+  -- Подсвечивает #ffffff
+  use 'ap/vim-css-color'
 
-	-----------------------------------------------------------
-	-- РАЗНОЕ
-	-----------------------------------------------------------
-	-- Даже если включена русская раскладка vim команды будут работать
-	use 'powerman/vim-plugin-ruscmd'
-	-- 'Автоформатирование' кода для всех языков
-	use 'Chiel92/vim-autoformat'
-	-- ]p - вставить на строку выше, [p - ниже
-	use 'tpope/vim-unimpaired'
-	-- Переводчик рус - англ
-	use 'skanehira/translate.vim'
-	--- popup окошки
-	use 'nvim-lua/popup.nvim'
-	-- Обрамляет или снимает обрамление. Выдели слово, нажми S и набери <h1>
-	use 'tpope/vim-surround'
-	-- Считает кол-во совпадений при поиске
-	use 'google/vim-searchindex'
-	-- Может повторять через . vimsurround
-	use 'tpope/vim-repeat'
-	-- Стартовая страница, если просто набрать vim в консоле
-	use 'mhinz/vim-startify'
-	-- Комментирует по gc все, вне зависимости от языка программирования
-	use { 'numToStr/Comment.nvim',
-	config = function() require('Comment').setup() end }
-	-- Обрамляет строку в теги по ctrl- y + ,
-	use 'mattn/emmet-vim'
-	-- Закрывает автоматом скобки
-	use 'cohama/lexima.vim'
-	-- Линтер, работает для всех языков
-	use 'dense-analysis/ale'
+  -----------------------------------------------------------
+  -- РАЗНОЕ
+  -----------------------------------------------------------
+  -- Даже если включена русская раскладка vim команды будут работать
+  use 'powerman/vim-plugin-ruscmd'
+  -- 'Автоформатирование' кода для всех языков
+  use 'Chiel92/vim-autoformat'
+  -- ]p - вставить на строку выше, [p - ниже
+  use 'tpope/vim-unimpaired'
+  -- Переводчик рус - англ
+  use 'skanehira/translate.vim'
+  --- popup окошки
+  use 'nvim-lua/popup.nvim'
+  -- Обрамляет или снимает обрамление. Выдели слово, нажми S и набери <h1>
+  use 'tpope/vim-surround'
+  -- Считает кол-во совпадений при поиске
+  use 'google/vim-searchindex'
+  -- Может повторять через . vimsurround
+  use 'tpope/vim-repeat'
+  -- Стартовая страница, если просто набрать vim в консоле
+  use 'mhinz/vim-startify'
+  -- Комментирует по gc все, вне зависимости от языка программирования
+  use { 'numToStr/Comment.nvim',
+  config = function() require('Comment').setup() end }
+  -- Обрамляет строку в теги по ctrl- y + ,
+  use 'mattn/emmet-vim'
+  -- Закрывает автоматом скобки
+  use 'cohama/lexima.vim'
+  -- Линтер, работает для всех языков
+  use 'dense-analysis/ale'
 
 end)
