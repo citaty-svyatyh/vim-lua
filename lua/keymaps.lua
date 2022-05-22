7290786ecd0e570fc94d0bcf0a0fde3c63e918d2
citaty-svyatyh <46151646+citaty-svyatyh@users.noreply.github.com>
2022-05-22 09:50:26 +0300

    Update keymaps.lua

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -27,8 +27,8 @@ map('n', '<C-a>', ':Files<CR>', default_opts)
 map('n', '<C-p>', '::Buffers<CR>', default_opts)
 
 -- telescope
--- map('n', '<C-a>', [[ <cmd>lua require('telescope.builtin').find_files()<cr> ]], default_opts)
--- map('n', '<C-p>', [[ <cmd>lua require('telescope.builtin').buffers()<cr> ]], default_opts)
+-- map('n', '<C-a>', [[ <cmd>lua require('telescope.builtin').find_files()<cr> ]], default_opts)                                                
+-- map('n', '<C-p>', [[ <cmd>lua require('telescope.builtin').buffers()<cr> ]], default_opts)                                                   
 -----------------------------------------------------------
 -- Фн. клавиши по F1 .. F12
 -----------------------------------------------------------
@@ -38,22 +38,22 @@ map('n', '<F1>', ':nohl<CR>', default_opts)
 map('n', '<S-F1>', ':g/^$/d<CR>', default_opts)
 -- <F2> для временной вставки из буфера, чтобы отключить авто идент
 vim.o.pastetoggle='<F2>'
--- <F3> перечитать конфигурацию nvim Может не работать, если echo $TERM  xterm-256color
+-- <F3> перечитать конфигурацию nvim Может не работать, если echo $TERM  xterm-256color                                                         
 map('n', '<F3>', ':so ~/.config/nvim/init.lua<CR>:so ~/.config/nvim/lua/plugins.lua<CR>:so ~/.config/nvim/lua/settings.lua<CR>:so ~/.config/nvim/lua/keymaps.lua<CR>', { noremap = true })
 -- <S-F3> Открыть всю nvim конфигурацию для редактирования
 map('n', '<S-F3>', ':e ~/.config/nvim/init.lua<CR>:e ~/.config/nvim/lua/plugins.lua<CR>:e ~/.config/nvim/lua/settings.lua<CR>:e ~/.config/nvim/lua/keymaps.lua<CR>', { noremap = true })
 -- <F4> Поиск слова под курсором
--- map('n', '<F4>', [[<cmd>lua require('telescope.builtin').grep_string()<cr>]], default_opts)
-map('n', '<F4>', ':Ack! <cword> --ignore-dir={static,logs,files}<CR>',  {noremap = true, silent = false})
+-- map('n', '<F4>', [[<cmd>lua require('telescope.builtin').grep_string()<cr>]], default_opts)                                                  
+map('n', '<F4>', ':Ack! <cword> --ignore-dir={static,logs,files}<CR>',  {noremap = true, silent = false})                                       
 -- <S-F4> Поиск слова (воскл. знак, чтобы не было автооткрытия файла)
-map('n', '<S-F4>', ':Ack! --ignore-dir={static,logs,files} ', {noremap = true, silent = false})
--- map('n', '<S-F4>', [[<cmd>lua require('telescope.builtin').live_grep()<cr>]], default_opts)
+map('n', '<S-F4>', ':Ack! --ignore-dir={static,logs,files} ', {noremap = true, silent = false})                                                 
+-- map('n', '<S-F4>', [[<cmd>lua require('telescope.builtin').live_grep()<cr>]], default_opts)                                                  
 -- <F5> разные вариации нумераций строк, можно переключаться
-map('n', '<F5>', ':exec &nu==&rnu? "se nu!" : "se rnu!"<CR>', default_opts)
+map('n', '<F5>', ':exec &nu==&rnu? "se nu!" : "se rnu!"<CR>', default_opts)                                                                     
 -- <S-F5> Показ окошка с ошибками от lsp и линтерово ale
-map('n', '<S-F5>', ':TroubleRefresh<CR>:TroubleToggle<CR>', default_opts)
+-- map('n', '<S-F5>', ':TroubleRefresh<CR>:TroubleToggle<CR>', default_opts)                                                                    
 -- <F6> дерево файлов.
-map('n', '<F6>', ':NvimTreeRefresh<CR>:NvimTreeToggle<CR>', default_opts)
+map('n', '<F6>', ':NvimTreeRefresh<CR>:NvimTreeToggle<CR>', default_opts)                                                                       
 -- <F8>  Показ дерева классов и функций, плагин majutsushi/tagbar
 map('n', '<F8>', ':TagbarToggle<CR>', default_opts)
 -- <F11> Проверка орфографии  для русского и английского языка
